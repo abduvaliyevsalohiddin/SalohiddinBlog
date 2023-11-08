07b994b88d11ed534f260e3b87d8a44060bdbb90
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-08 01:28:26 -0800

    SalohiddinBlog

---
+++ AzimjonBlog/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from asosiy.views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('about/', about),
+    path('blog/', blog),
+    # path('maqola/<str:slug>/', maqola),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ asosiy/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import *
+
+
+def home(requests):
+    return render(requests, "home.html")
+
+
+def about(requests):
+    return render(requests, "about.html")
+
+
+def blog(requests):
+    content = {
+        "maqolalar": Maqola.objects.all()
+    }
+    return render(requests, "blog.html", content)
+
+
+# def maqola(requests, slug):
+#     content = {
+#         "maqola": Maqola.objects.filter(slug=slug)
+#     }
+#     return render(requests, "maqola.html",content)
